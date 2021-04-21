baed2a841f8ee6408f8c621912443be009abcac1
Lukas Mattsson <lu7511ma-s@student.lu.se>
2021-04-21 14:28:37 +0200

    Changed module and app name

--- app/Digipong/src/main/java/com/example/digipong/MainActivity.java
+++ app/Digipong/src/main/java/com/example/digipong/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.digipong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/Digipong/src/test/java/com/example/digipong/ExampleUnitTest.java
+++ app/Digipong/src/test/java/com/example/digipong/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.digipong;
 
 import org.junit.Test;
 
