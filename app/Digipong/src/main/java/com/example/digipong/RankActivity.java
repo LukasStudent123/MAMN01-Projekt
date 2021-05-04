8bd4725f2dfec754c03999c1af20c22a458f7b7d
emmafranzon <emma.franzon@gmail.com>
2021-05-04 14:46:42 +0200

    Initial work

--- app/Digipong/src/main/java/com/example/digipong/RankActivity.java
+++ app/Digipong/src/main/java/com/example/digipong/RankActivity.java
@@ -2,22 +2,32 @@ package com.example.digipong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RankActivity extends AppCompatActivity {
     private ImageView img;
+    private TextView one;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
 
+        //Formatera i XML
+        one = (TextView) findViewById(R.id.one);
+        one.setBackgroundColor(Color.rgb(245, 179, 0));
+
+        //To-do lägg till användare i array och printa ut i textview
+        one.setText("1: " + name);
+
     }
-    //to-do
 
 
 
