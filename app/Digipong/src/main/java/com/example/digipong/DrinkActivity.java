23c3b96eca5d645087059ffd01d0689cd41702bc
emmafranzon <emma.franzon@gmail.com>
2021-05-22 14:20:57 +0200

    Drinkactivity layout done and sound added.

--- app/Digipong/src/main/java/com/example/digipong/DrinkActivity.java
+++ app/Digipong/src/main/java/com/example/digipong/DrinkActivity.java
@@ -1,5 +1,6 @@
 package com.example.digipong;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,16 +9,35 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
 public class DrinkActivity extends AppCompatActivity {
     private ImageView cup;
+    private MediaPlayer mp;
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        cup = (ImageView) findViewById(R.id.cup);
+        mp = MediaPlayer.create(this, R.raw.drinking);
+        mp.start();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mp.stop();
+                finish();
+            }
+        }, 4000);
+
+
 
 
 
